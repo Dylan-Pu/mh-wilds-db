c0335fb8d2d66e6c1047a18c90a8cec74e72de9f
Dylan-Pu <putzoludylan@gmail.com>
2025-11-29 19:40:16 +0100

    Reset tracking

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Controllers\MonsterController; 
-use App\Controllers\TestAPI;
-
-Route::get('/', [MonsterController::class, 'index'])->name('home');
-Route::get('/monster/{slug}', [MonsterController::class, 'show'])->name('monster.show');
