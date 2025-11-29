392faefd0cc0f89481b31cf0246ca9c1a0f1e548
Dylan-Pu <putzoludylan@gmail.com>
2025-11-29 22:23:05 +0100

    maj

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,11 +14,15 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
+
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+        if ($this->app->environment('production')) {
+
+            URL::forceScheme('https');
+        }
     }
 }
