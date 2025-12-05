ad635269d361dc88e90ce5ba9efac1e726b98540
Dylan-Pu <putzoludylan@gmail.com>
2025-12-05 20:12:10 +0100

    allez on y va!

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,12 +3,16 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; // <- nécessaire pour DB::table()
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // On lance UNIQUEMENT notre seeder MH Wilds
+        // On vide la table monsters avant de reseeder
+        DB::table('monsters')->truncate();
+
+        // On lance notre seeder MH Wilds
         $this->call(MonsterHunterWildsSeeder::class);
     }
 }
